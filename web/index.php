fb0774ab9d091617a916232354669a88bdd7c5ed
Kerttu Lumi <kerttu.lumi@khk.ee>
2018-09-18 15:15:59 +0300

    use composer psr-4 autoload, create a class application.php

--- web/index.php
+++ web/index.php
@@ -1,45 +1,10 @@
 <?php
-// web/index.php
+//web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use BookingApp\Application;
 
-$app['debug'] = true;
-
-$app->register(new Silex\Provider\TwigServiceProvider(), [
-	'twig.path' => __DIR__.'/../views',
-]);
-
-$app->get('/bookings/create', function () use ($app) {
-	return $app ['twig']->render('base.html.twig');
-});
-
-$app->register(new Silex\Provider\DoctrineServiceProvider(), [
-	'db.options' => [
-
-		'driver' => 'pdo_sqlite',
-
-		'path' => __DIR__.'/../database/app.db',
-	],
-]);
-
-// Creating a table if it doesn't exist yet
-if (!$app['db']->getSchemaManager()->tablesExist('bookings')) {
-	$app['db']->executeQuery("CREATE TABLE bookings (
-		id INT UNSIGNED AUTO INCREMENT PRIMARY KEY,
-		firstName VARCHAR(40) NOT NULL,
-		lastname VARCHAR(40) NOT NULL,
-		phone VARCHAR(10) NOT NULL,
-		email VARCHAR(20) DEFAULT NULL,
-		birthday DATE NOT NULL,
-		startDate DATE NOT NULL,
-		endDate DATE NOT NULL,
-		arrivalTime TIME DEFAULT NULL,
-		additionalInformation TEXT,
-		nrOfPeople INT NOT NULL,
-		payingMethod VARCHAR(10) NOT NULL
-		);");
-}
+$app = new Application();
 
 $app->run();
 ?>

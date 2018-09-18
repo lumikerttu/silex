58b0001557d444bafe448ec5602076da137760b2
Kerttu Lumi <kerttu.lumi@khk.ee>
2018-09-18 10:53:16 +0300

    Create front-controller index.php and bootstrap Silex

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<?php
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/hello', function()  {
+	return 'Hello world!';
+});
+// ... definitions
+
+$app->run();
+?>
